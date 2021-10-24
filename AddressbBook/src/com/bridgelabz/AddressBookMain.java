1482b4ab2f97f8eaf86b288d60eb132f2dfcf7ec
prashant169 <prashantphad2018@gmail.com>
2021-10-23 23:28:34 -0800

    Added:Program for create contact in addressbook

---
+++ AddressbBook/src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+	public static void main(String args[]) {
+		System.out.println("welcome to AddressBook Sysetm program ");
+		ContactDetail newContact = new ContactDetail();
+		newContact.setFirstName("prasjhant\n");
+		newContact.setLastName("phad\n");
+		newContact.setAddressCity("Ambajogai\n");
+		newContact.setState("maharashatra\n");
+		newContact.setEmail("prashantPhad2018@gmail.com");
+
+	}
+}
